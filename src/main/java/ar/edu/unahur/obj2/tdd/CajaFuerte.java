fc5e0a1317698e305160d63f9f7df8d431ff0e07
jerebryne <jeremiasmarkevich@gmial.com>
2025-04-23 02:09:51 -0300

    Realizado¿

--- src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
+++ src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
@@ -1,5 +1,28 @@
 package ar.edu.unahur.obj2.tdd;
 
 public class CajaFuerte {
-
-}
+	
+	private Boolean abierta = true;
+	private Integer codigo = null;
+	
+	public Boolean estaAbierta() {
+		return abierta;
+	}
+	
+	public Integer codigo() {
+		return this.codigo;
+	}
+	
+	public void cerrar(Integer codigo) {
+		this.abierta = false;
+		this.codigo = codigo;
+	}
+	
+	public void abrir(Integer codigo) {
+		if(this.codigo.equals(codigo)) {
+			this.abierta = true;
+		} else {
+			throw new RuntimeException("El codigo ingresado es incorrecto.");
+		}
+	}
+} 
--- src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
+++ src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
@@ -1,10 +1,39 @@
 package ar.edu.unahur.obj2.tdd;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class CajaFuerteTest {
-
+	@Test
+	public void laCajaFuertaEstaAbiertaAlCrearse() {
+		CajaFuerte caja = new CajaFuerte();
+		assertTrue(caja.estaAbierta());
+	}
+	
+	@Test
+	public void queSeAbreLaCajaFuerteConElCodigoCorrecto() {
+		CajaFuerte caja = new CajaFuerte();
+		caja.cerrar(1234);
+		caja.abrir(1234);
+		assertTrue(caja.estaAbierta());
+	}
+	
+	@Test
+	public void sePuedeCerrarEstableciendoUnCodigoNuevo() {
+		CajaFuerte caja = new CajaFuerte();
+		caja.cerrar(1234);
+		assertEquals(caja.codigo(),1234);
+	}
+	
+	@Test
+	public void queLanceErrorAlAbrirConUnCodigoIncorrecto() {
+		CajaFuerte caja = new CajaFuerte();
+		caja.cerrar(1234);
+		assertThrows(RuntimeException.class, () -> {
+            caja.abrir(4321);});
+	}
 }
